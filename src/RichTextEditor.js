436169517778985c2bf49dc69d2c946ea3973ec6
yudingp <yudingp@163.com>
2018-12-25 18:16:37 +0800

    fix RichTextEditor keyboard height

--- src/RichTextEditor.js
+++ src/RichTextEditor.js
@@ -298,7 +298,7 @@ export default class RichTextEditor extends Component {
     const {bottomSpacing = 0} = this.props
     //in release build, external html files in Android can't be required, so they must be placed in the assets folder and accessed via uri
     const pageSource = PlatformIOS ? require('./editor.html') : { uri: 'file:///android_asset/editor.html' };
-    const rootStyle = PlatformIOS ? { flex: 1 } : { flex: 1, marginBottom: (keyboardHeight + bottomSpacing) }
+    const rootStyle = PlatformIOS ? { flex: 1 } : { flex: 1, marginBottom: bottomSpacing }
     return (
       <View style={rootStyle}>
         <WebViewBridge
